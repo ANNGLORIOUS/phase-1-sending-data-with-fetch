b306185b871bf19a58c72292fe3761c938df3534
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-07-14 13:23:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+  
